9999242299eaaafc06a0730b0d0834d9ffaf0a53
ebrovkova <114899620+ebrovkova@users.noreply.github.com>
2023-04-29 17:45:02 +0300

    Update main.c

--- main.c
+++ main.c
@@ -15,65 +15,94 @@ Node *create_node(){
 }
 
 
-void clear_mem(Node *s){
+void clear_mem(Node *pointer){
     Node *tmp;
-    while (s != NULL){
-        tmp = s->next;
-        free(s);
-        s = tmp;
+    while (pointer != NULL){
+        tmp = pointer->next;
+        free(pointer);
+        pointer = tmp;
     }
 }
 
 
-int bin_to_dec(char *b){
+int bin_to_dec(char *bin_str){
     int dec = 0;
-
-    while (*b) {
+    while (*bin_str) {
         dec *= 2;
-        if (*b++ == '1')
+        if (*bin_str++ == '1')
             dec += 1;
     }
-
     return dec;
 }
 
 
-int bin_check(char* n) {
-    int length = (int) strlen(n);
+int bin_check(char* number) {
+    int length = (int) strlen(number);
     short bin = 1;
     for (int i = 0; i<length; i++) {
-        if (n[i] != '0' && n[i] != '1')
+        if (number[i] != '0' && number[i] != '1')
             bin = 0;
     }
     return bin;
 }
 
 
-Node *add(Node *s){
-    char *b;
-    scanf("%as", &b);
-    if (!bin_check(b)) return s;
+Node *add(Node *pointer){
+    char *bin_str;
+    scanf("%as", &bin_str);
+    if (!bin_check(bin_str)) return pointer;
 
-    Node *t = create_node();
-    t->bin = b;
-    t->dec = bin_to_dec(b);
-    t->next = NULL;
+    Node *new_node = create_node();
+    new_node->bin = bin_str;
+    new_node->dec = bin_to_dec(bin_str);
+    new_node->next = NULL;
 
-    if (s == NULL) return t;
+    if (pointer == NULL) return new_node;
 
-    Node *h = s;
-    while (s->next != NULL){
-        if (s->bin == t->bin){
-            free(t);
-            return h;
+    Node *head = pointer;
+    while (pointer->next != NULL){
+        if (pointer->bin == new_node->bin){
+            free(new_node);
+            return head;
         }
-        s = s->next;
+        pointer = pointer->next;
     }
-    if (s->bin == t->bin){
-        free(t);
-        return h;
+    if (pointer->bin == new_node->bin){
+        free(new_node);
+        return head;
     }
-    s->next = t;
+    pointer->next = new_node;
+    return head;
+}
+
+
+void print_set(Node *pointer){
+    while (pointer != NULL){
+        printf("%s %d\n", pointer->bin, pointer->dec);
+        pointer = pointer->next;
+    }
+}
+
+
+int main(void){
+    printf("Type 1 to add element\nType 2 to print set\nType 0 to exit\n");
+    Node *set = NULL;
+
+    int action;
+    scanf("%d", &action);
+    while (action != 0){
+        if (action == 1){
+            set = add(set);
+        } else if (action == 2){
+            print_set(set);
+            printf("\n");
+        }
+        scanf("%d", &action);
+    }
+
+    clear_mem(set);
+    return 0;
+}
     return h;
 }
 
